f90ff9b89ba963e1eb73414215db9bd4bb0e0fba
Zorbik <37942049+Zorbik@users.noreply.github.com>
2022-12-11 20:20:53 +0000

    Deploying to gh-pages from @ Zorbik/goit-react-hw-08-phonebook@ab5075ec12061db71d3fea8dbc5a96cd8565e902 🚀

--- static/js/680.9be2ec54.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[680],{680:function(e,t,n){n.r(t);var o=n(4567),s=n(3329);t.default=function(){return(0,s.jsx)(o.xu,{as:"main",display:"flex",justifyContent:"center",children:(0,s.jsx)(o.xu,{as:"h1",textAlign:"center",mt:6,children:"Hello! It's just a phonebook"})})}}}]);
-//# sourceMappingURL=680.9be2ec54.chunk.js.map
---
+++ static/js/974.9e03a3f5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[974],{4974:function(e,t,n){n.r(t);var o=n(7241),u=n(3329);t.default=function(){return(0,u.jsx)(o.xu,{display:"flex",justifyContent:"center",my:6,children:(0,u.jsx)(o.GG,{})})}}}]);
+//# sourceMappingURL=974.9e03a3f5.chunk.js.map
