63bf7277ab4326cff9852963ba10c0015bda1ace
Zorbik <37942049+Zorbik@users.noreply.github.com>
2022-12-11 10:39:45 +0000

    Deploying to gh-pages from @ Zorbik/goit-react-hw-08-phonebook@d14d334b25b72568b0b9c8e632ac5434af1f71b9 🚀

---
+++ static/js/680.9f5cf3b0.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[680],{680:function(e,t,n){n.r(t);var o=n(7525),s=n(3329);t.default=function(){return(0,s.jsx)(o.xu,{as:"main",display:"flex",justifyContent:"center",children:(0,s.jsx)(o.xu,{as:"h1",textAlign:"center",mt:6,children:"Hello! It's just a phonebook"})})}}}]);
+//# sourceMappingURL=680.9f5cf3b0.chunk.js.map
